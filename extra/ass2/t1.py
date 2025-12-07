64f1e1ae866d11f27de98270330b85241a83eb91
root <root@LAPTOP-0GOGN00R.localdomain>
2025-12-07 11:31:02 +0100

    t2

---
+++ extra/ass2/t1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Std. no: 2711498
+'''
+
+from typing import List
+
+
+def smallest_subsequence_length(array: List[int], k: int) -> int:
+    '''
+    Find the length of the smallest sub-array of `array`
+        whose sum of elements is greater than `k`
+
+    ### Parameters
+    `array`: An array of positive integers
+
+    `k`: int
+
+    ### Return
+    The length of the smallest sub-array
+    '''
