fec9328f2b0cb3398507f80b6781b0f783999ca5
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-12-14 11:00:09 +0100

    Create 2_2_4_modify_sorting_goodbestcase.py

---
+++ tutorial/t3/2_2_4_modify_sorting_goodbestcase.py
@@ -0,0 +1,9 @@
+
+"""
+Modify the algorithm so that it first checks the input array
+to see whether it is already sorted, taking Theta(n) time for an 
+n-element array. 
+If the array is already sorted, then the algorithm is done, Otherwise,
+sort the array as usual. The best-case running time is generally not 
+a good measure of an algorithm's efficiency.
+"""
